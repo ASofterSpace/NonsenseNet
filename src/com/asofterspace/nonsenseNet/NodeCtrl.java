0b2a377d91d2910bea07514aa3a12d93d8bb4b34
Tom Moya Schiller <moya@asofterspace.com>
2020-02-04 02:36:25 +0100

    Prepare for correct initialization

--- src/com/asofterspace/nonsenseNet/NodeCtrl.java
+++ src/com/asofterspace/nonsenseNet/NodeCtrl.java
@@ -12,24 +12,33 @@ import java.util.List;
 
 public class NodeCtrl {
 
-	private int NODE_AMOUNT = 100;
+	private int NODES_PER_LEVEL = 100;
+	private int LEVELS = 5;
+	private int CALCULATION_ROUNDS = 10;
 
-	private List<GenericNode> nodes;
+	private List<Node> nodes;
 
 
 	public NodeCtrl() {
 
 		this.nodes = new ArrayList<>();
 
-		for (int i = 0; i < NODE_AMOUNT; i++) {
-			nodes.add(new Node());
+		for (int level = 0; level < LEVELS; level++) {
+			for (int i = 0; i < NODES_PER_LEVEL; i++) {
+				Node node = new Node();
+				// add some random ins and outs, and some random combinators, operations and splitters
+				// node.addInput();
+				nodes.add(node);
+			}
 		}
 	}
 
 	public void run() {
 
-		for (GenericNode node : nodes) {
-			node.calculate();
+		for (int i = 0; i < CALCULATION_ROUNDS; i++) {
+			for (Node node : nodes) {
+				node.calculate();
+			}
 		}
 	}
 
