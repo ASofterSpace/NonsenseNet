97c209e7eb89f91fc947eaa58303a90bec14085c
Tom Moya Schiller <moya@asofterspace.com>
2020-05-06 22:52:15 +0200

    Add some thoughts and ideas on future developments

--- src/com/asofterspace/nonsenseNet/Main.java
+++ src/com/asofterspace/nonsenseNet/Main.java
@@ -33,6 +33,35 @@ public class Main {
 			}
 		}
 
+		/*
+		General plan:
+		Right now, there is just one node ctrl here, so basically one NonsenseNet
+		We want to:
+		1) Get that one to work at least a liiiittle bit
+		2) Create the first tiny "organism" in a simulated 2D environment, let's say amoeba,
+		   with a NonsenseNet inside, which just floats around and acts on stimuli such as
+		   food: nom!
+		3) Add more of them, let's say at least have 100 amoeba floating around doing their
+		   thing, ideally with reproduction and evolving NonseneNets
+		4) Add new, more advanced organisms, let's say ants, which crawl around 2D space
+		   with intention and in addition to eating and reproducing also solve puzzles
+		   together
+		5) Finally, add even more advanced organisms that exist in a 3D space and see the 3D
+		   space through eyes which get a generated view of their environment as input
+		   (so yeah the server knows about the absolute positions and body shapes and so on
+		   of everything, and generates 2D images for their eyes to see, and their NonsenseNets
+		   then take these images and try to understand where everything is in the space etc. xD)
+		6) Add a new 3D avatar for us as human to interact with the advanced organisms
+		*/
+
+		/*
+		Random notes:
+		* Regarding memory, it would be great to have fuzzy memory that groups similar
+		  events and forgets about less important things etc... in general, we want to
+		  imitate life as we know it as well as we can, hoping that if we do this well
+		  enough, just magically awesomeness arises :D
+		*/
+
 		NodeCtrl nodeCtrl = new NodeCtrl();
 		nodeCtrl.run();
 
