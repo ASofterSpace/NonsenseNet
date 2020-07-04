3115cd399d5d9e4f1f62bab9133c41698f75eff5
Tom Moya Schau <moya@asofterspace.com>
2020-07-04 22:33:28 +0200

    Add function skeleton

--- src/com/asofterspace/nonsenseNet/Listener.java
+++ src/com/asofterspace/nonsenseNet/Listener.java
@@ -10,4 +10,20 @@ package com.asofterspace.nonsenseNet;
  */
 public class Listener {
 
+	/**
+	 * Takes some audio and extracts the text spoken within it
+	 */
+	public String extractTextFromAudio(WavFile inputFile) {
+		return "";
+	}
+
+	/**
+	 * Starts some more training rounds, during which the listener tries to get
+	 * better using pre-defined examples, while being supervised by the ListenerTrainer
+	 * who gets input from the user
+	 */
+	public void startTraining() {
+
+	}
+
 }
---
+++ src/com/asofterspace/nonsenseNet/ListenerTrainer.java
@@ -0,0 +1,18 @@
+/**
+ * Unlicensed code created by A Softer Space, 2020
+ * www.asofterspace.com/licenses/unlicense.txt
+ */
+package com.asofterspace.nonsenseNet;
+
+
+/**
+ * Trains the listener by scoring its results for its inputs, and if the trainer
+ * itself is confused, asks the user about input
+ */
+public class ListenerTrainer {
+
+	public double scoreListening(WavFile inputFile, String extractedText) {
+		return 0;
+	}
+
+}
